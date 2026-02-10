6ddee4a8395637a8435d862b6659a62c96361d0d
mohmadamine <professionnelmohmad@gmail.com>
2026-02-10 22:54:52 +0100

    format document

--- include/Processing_Lib.h
+++ include/Processing_Lib.h
@@ -1,20 +1,25 @@
 #pragma once
 
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
 namespace processing_numbers
 {
-int reversed_number(int number){
-short remainder = 0;
-int number2 = 0;
-while(number>0){
-remainder = number%10;
-number2 = number2*10 + remainder;
-number = number/10;
-}
-return number2;
+    int reversed_number(int number)
+    {
+        short remainder = 0;
+        int number2 = 0;
+        while (number > 0)
+        {
+            remainder = number % 10;
+            number2 = number2 * 10 + remainder;
+            number = number / 10;
+        }
+        return number2;
+    }
+
 
 }
-}
+
+
