f226f675fe322192c4e7a350dd0a7cc5ec30d751
mohmadamine <professionnelmohmad@gmail.com>
2026-02-11 22:43:22 +0100

    add check_number_is_palindrome_or_not() to check_lib.h

--- include/check_lib.h
+++ include/check_lib.h
@@ -1,42 +1,55 @@
 #pragma once
 
-#include<iostream> 
-#include<cmath>
+#include <iostream>
+#include <cmath>
+#include "Processing_Lib.h"
 
 using namespace std;
 
 namespace check_lib
 {
-    bool is_perfect_number(int number){
-    int sum =0;
-    for(int i = 1 ; i<number;i++){
-    if(number%i==0){
-        sum+=i;
-    }
-
+  bool is_perfect_number(int number)
+  {
+    int sum = 0;
+    for (int i = 1; i < number; i++)
+    {
+      if (number % i == 0)
+      {
+        sum += i;
+      }
     }
     return number == sum;
-}
+  }
 
-enum enprim_number{
-    yes = 1,no
-};
+  enum enprim_number
+  {
+    yes = 1,
+    no
+  };
 
-enprim_number check_prime_number(int number){
-  
-   int M = round(number/2);
-    int i =2;
-    while(i<=M){
+  enprim_number check_prime_number(int number)
+  {
 
-      if(number%i==0){
-           return enprim_number::no;
+    int M = round(number / 2);
+    int i = 2;
+    while (i <= M)
+    {
+
+      if (number % i == 0)
+      {
+        return enprim_number::no;
       }
-        i++;
-      }
-       
-     
-        return enprim_number::yes;
-}
+      i++;
+    }
 
+    return enprim_number::yes;
+  }
+
+
+
+  bool check_number_is_palindrome_or_not(int number)
+  {
+    return number == processing_numbers::reversed_number(number);
+  }
 
 }
